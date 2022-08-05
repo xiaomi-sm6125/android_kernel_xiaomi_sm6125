81d56a76cb691d9f6532a34e3743e8ab28635a5c
Ivan Vecera <ivan@cera.cz>
2022-08-05 20:16:49 +0200

    drm/msm/dsi-staging: Use doze mode commands during low-power mode
    Currently when AOD is enabled the panel is not switched into low-power
    mode because there is no lp1/lp2 dsi commands defined. On other side
    there are doze-hbm and doze-lbm commands that could be used instead.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Ide5377c4fe2bef88322c1050cebb850f2f5db34d

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1067,8 +1067,10 @@ u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 	u32 brightness = dsi_panel_get_backlight(panel);
 	int i;
 
-	/* No dimming is required if HBM mode is enabled */
-	if (panel->hbm_enabled)
+	/* No dimming is required if HBM mode is enabled and device
+	 * is not in doze mode.
+	 */
+	if (panel->hbm_enabled && !panel->doze_status)
 		return 0;
 
 	if (!panel->fod_dim_lut)
@@ -1091,11 +1093,35 @@ u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 {
-	/* If HBM is enabled by user do nothing */
-	if (panel->hbm_enabled)
-		return 0;
+	int rc = 0;
 
-	return dsi_panel_set_hbm(panel, status);
+	if (status) {
+		/* Switch to HBM mode if:
+		 * - it is not already enabled by user
+		 * - we're coming from doze mode
+		 */
+		if (!panel->hbm_enabled || panel->doze_status)
+			rc = dsi_panel_set_hbm(panel, true);
+	} else {
+		if (!panel->doze_status) {
+			/* Switching back to normal mode */
+
+			/* Switch-off HBM mode if it is not enabled by user */
+			if (!panel->hbm_enabled)
+				rc = dsi_panel_set_hbm(panel, false);
+
+			return rc;
+		}
+		/* Switching back to doze mode */
+		if (panel->hbm_enabled)
+			rc = DSI_PANEL_SEND(panel,
+					    DISP_HBM_FOD_OFF_DOZE_HBM_ON);
+		else
+			rc = DSI_PANEL_SEND(panel,
+					    DISP_HBM_FOD_OFF_DOZE_LBM_ON);
+	}
+
+	return rc;
 }
 
 int dsi_panel_set_hbm_enabled(struct dsi_panel *panel, bool status)
@@ -2131,6 +2157,8 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-doze-lbm-on-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command",
+	"qcom,mdss-dsi-doze-hbm-command",
+	"qcom,mdss-dsi-doze-lbm-command",
 #endif
 };
 
@@ -2183,6 +2211,8 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-doze-lbm-on-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command-state",
+	"qcom,mdss-dsi-doze-hbm-command-state",
+	"qcom,mdss-dsi-doze-lbm-command-state",
 #endif
 };
 
@@ -3898,6 +3928,7 @@ struct dsi_panel *dsi_panel_get(struct device *parent,
 		return ERR_PTR(-ENOMEM);
 
 	panel->panel_of_node = of_node;
+	panel->doze_status = false;
 	panel->parent = parent;
 	panel->type = type;
 
@@ -4008,6 +4039,8 @@ struct dsi_panel *dsi_panel_get(struct device *parent,
 #endif
 
 	panel->power_mode = SDE_MODE_DPMS_OFF;
+	panel->doze_status = false;
+
 	drm_panel_init(&panel->drm_panel);
 
 #ifdef CONFIG_MACH_XIAOMI_F9S
@@ -4498,6 +4531,17 @@ error:
 	return rc;
 }
 
+int dsi_panel_enable_doze(struct dsi_panel *panel)
+{
+	/* Select doze mode according HBM state */
+	if (panel->hbm_enabled)
+		/* HBM enabled -> use HBM doze mode */
+		return DSI_PANEL_SEND(panel, DOZE_HBM);
+
+	/* HBM disabled -> use normal doze mode */
+	return DSI_PANEL_SEND(panel, DOZE_LBM);
+}
+
 int dsi_panel_set_lp1(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -4533,6 +4577,11 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 		    panel->name, rc);
 
 #ifdef CONFIG_MACH_XIAOMI_F9S
+	rc = dsi_panel_enable_doze(panel);
+	if (rc)
+		pr_err("[%s] unable to enable doze mode, rc=%d\n",
+		       panel->name, rc);
+
 	if (panel->fod_hbm_enabled || panel->fod_backlight_flag) {
 		pr_info("skip doze backlight,[hbm=%d][fod_bl=%d]\n",
 			panel->fod_hbm_enabled, panel->fod_backlight_flag);
@@ -4570,6 +4619,12 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 	if (rc)
 		pr_err("[%s] failed to send DSI_CMD_SET_LP2 cmd, rc=%d\n",
 		       panel->name, rc);
+
+	rc = dsi_panel_enable_doze(panel);
+	if (rc)
+		pr_err("[%s] unable to enable doze mode, rc=%d\n",
+		       panel->name, rc);
+
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -5139,6 +5194,7 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	}
 	panel->panel_initialized = false;
 #ifdef CONFIG_MACH_XIAOMI_F9S
+	panel->doze_status = false;
 	panel->fod_hbm_enabled = false;
 	panel->fod_backlight_flag = false;
 	panel->dimming_enabled = false;
